c6f129771b77f4adf6ef3a64a510697cfd248eda
Edward Kamau <teddykamaa@gmail.com>
2024-05-19 13:13:59 +0300

    Create create_account.rs

---
+++ create_account.rs
@@ -0,0 +1,21 @@
+pub fn create_account(
+    from_pubkey: &Pubkey,
+    to_pubkey: &Pubkey,
+    lamports: u64,
+    space: u64,
+    owner: &Pubkey,
+) -> Instruction {
+    let account_metas = vec![
+        AccountMeta::new(*from_pubkey, true),
+        AccountMeta::new(*to_pubkey, true),
+    ];
+    Instruction::new_with_bincode(
+        system_program::id(),
+        &SystemInstruction::CreateAccount {
+            lamports,
+            space,
+            owner: *owner,
+        },
+        account_metas,
+    )
+}
